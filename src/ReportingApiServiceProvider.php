7aac5e69ccdba3730ae111ac3abd537734abd6e4
Audun Rundberg <audun@rundberg.no>
2026-05-03 10:05:19 +0200

    feat: add middleware to set Reporting-Endpoints response header
    Registers an 'reporting-endpoints' middleware alias that emits
    'Reporting-Endpoints: default="<path>"' so browsers know where to
    POST their W3C Reporting API and CSP reports.

--- src/ReportingApiServiceProvider.php
+++ src/ReportingApiServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace audunru\ReportingApi;
 
+use audunru\ReportingApi\Http\Middleware\AddReportingEndpointsHeader;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -13,5 +14,10 @@ class ReportingApiServiceProvider extends PackageServiceProvider
             ->name('reporting-api')
             ->hasConfigFile()
             ->hasRoutes('reporting-api');
+    }
+
+    public function packageBooted(): void
+    {
+        $this->app['router']->aliasMiddleware('reporting-endpoints', AddReportingEndpointsHeader::class);
     }
 }
